369e984e2a6eb2a8cb2eaf825f3b2a93779c43c5
Casiuss <casiuss@me.com>
2016-06-07 20:01:47 +0200

    change loop
    changed loop so it exits after n loops

--- Python/plant_moisture_lvl.py
+++ Python/plant_moisture_lvl.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
 
-def read_sensor(sensor,interval):
+def read_sensor(readouts,sensor,interval):
     import time
     from time import gmtime, strftime
     import grovepi
 
-    while True:
+    while readouts > 0:
         try:
             print("{}, {}".format(strftime("%d-%m-%Y, %H:%M:%S", gmtime()), grovepi.analogRead(sensor)))
             time.sleep(interval)
+            readouts=readouts-1
         except KeyboardInterrupt:
             break
         except IOError:
@@ -32,9 +33,16 @@ def main():
                     type=int,
                     metavar='<int>',
                     help='Set readout interval. Default is: [%(default)s]')
+    parser.add_argument('-r', '--readouts',
+                    dest='readouts',
+                    action='store',
+                    default='5',
+                    type=int,
+                    metavar='<int>',
+                    help='Specify how many readouts you want. Default is: [%(default)s]')
     args = parser.parse_args()
 
-    read_sensor(args.sensor,args.interval)
+    read_sensor(args.readouts,args.sensor,args.interval)
 
 
 if __name__=='__main__':
